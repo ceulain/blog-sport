092b2ad90ed9b69dac77346d8de10c8f8a4b5076
cbansimba <ceulain.bansimba@canal-plus.com>
2019-07-15 18:51:02 +0200

    tutorial gastby

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React from "react"
 import headerStyles from "./styles/header.module.scss"
-
+import { graphql, useStaticQuery } from 'gatsby'
 const itemList = ["Accueil", "A propos", "Contact"]
 
 const buildList = () =>
@@ -10,12 +10,23 @@ const buildList = () =>
 const Header = () => {
   console.log(headerStyles)
 
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
   return (
     <header className={headerStyles.header_container}>
-        <nav>
-          <ul className={headerStyles.ul}>{buildList()}</ul>
-        </nav>
-        <h1>Boost</h1>
+      <nav>
+        <ul className={headerStyles.ul}>{buildList()}</ul>
+      </nav>
+      <h1>{data.site.siteMetadata.title}</h1>
     </header>
   )
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,15 @@
 import React from "react"
 import "../styles/global.scss"
 import Header from "./header"
+import { Link } from "gatsby"
 
 export default ({ children }) => (
   <div>
+    <Link
+      to={`/about/`}
+    >
+      About
+    </Link>
     <Header />
     {children}
   </div>
